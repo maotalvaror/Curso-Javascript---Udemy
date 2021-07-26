96bd3499cbd971f1250b7210aba5d392345940f2
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-07-26 17:02:55 -0500

    Recibe la cantidad de Palabras que digite el usuario

---
+++ DFD/7. programa que recibe una cantidad de palabras definidas por el usuario.js
@@ -0,0 +1,8 @@
+console.log('programa que recibe una cantidad de palabras definidas por el usuario')
+var cantidadPalabras = prompt('Digite cuantas palabras desee');
+var iteraciones = parseInt(cantidadPalabras);
+
+for (let i = 1; i <= iteraciones; i++) {
+ const array = prompt('Digitelas')
+ console.log(array)
+}
