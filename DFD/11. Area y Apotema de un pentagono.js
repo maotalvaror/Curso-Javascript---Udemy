12ba9306c84edc2c903841170b19550e37463cc9
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-07-29 17:01:22 -0500

    Area y apotema Del pentagono

---
+++ DFD/11. Area y Apotema de un pentagono.js
@@ -0,0 +1,6 @@
+console.log('Programa que me imprime el area del pentagono dependiendo su lado y su apotema');
+var ladoPentagono = parseInt(prompt('Digite el lado'));
+var apotema = parseInt(prompt('Digite el apotema'));
+
+var area =(5*ladoPentagono*apotema)/2;
+console.log('Area del pentagono = ',area)
