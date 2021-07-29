e95274220bbeba49b47cb20172db2551d59a4ca7
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-07-29 17:12:10 -0500

    Area de un triangulo por su Base y Altura

---
+++ DFD/12. Area de un triangulo.js
@@ -0,0 +1,6 @@
+console.log('Programa que imprima el area de un triangulo dependiendo la base y altura');
+var base = parseInt(prompt('Digite la base'));
+var altura = parseInt(prompt('Digite la altura'));
+
+var area = (base*altura)/2;
+console.log('El area es = ',area)
