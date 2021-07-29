45068b084d6a1034aec6e27b65f8c4acdc8f6376
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-07-29 16:46:24 -0500

    Area de un circulo

---
+++ DFD/10. Area de un cuadrado.js
@@ -0,0 +1,7 @@
+console.log('Programa que me imprime el area de un cuadrado dependiendo el lado');
+var lado = prompt('Digite el lado');
+var _lado = parseInt(lado);
+
+var area = _lado ** 2;
+
+console.log('El area del cuadrado es = ',area)
