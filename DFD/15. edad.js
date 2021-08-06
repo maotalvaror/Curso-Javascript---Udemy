77cf504b19315039a4570a21c359b2be41ce1c24
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-08-06 18:03:49 -0500

    Si es mayor o menor

---
+++ DFD/15. edad.js
@@ -0,0 +1,8 @@
+console.log('Programa para saber si una persona es mayor de edad');
+const edadUsuario = parseInt(prompt('Digite su edad'));
+
+if(edadUsuario >= 18){
+  console.log('Si es mayor de edad');
+}else{
+  console.log('Es menor de edad')
+}
