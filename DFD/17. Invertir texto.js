abf8623e168653a41c896f696dcee4410f05025d
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-08-17 14:40:18 -0500

    Invertir texto de usuario

--- DFD/17. Invertir texto.js
+++ DFD/17. Invertir texto.js
@@ -1,14 +1,33 @@
 console.log('Este programa invierte el texto digitado por usuario');
 const cadenaTexto =prompt('Digite cadena de texto');
+let cadenaInvertida = '';
+let longitudCadena = cadenaTexto.length; 
+let ConteoRegresivo = longitudCadena;
 
-const longitudCadena = cadenaTexto.length;
-console.log('La longitud es de ',longitudCadena);
+console.log('Longitud: ',longitudCadena);
+for(let i = 0; i < longitudCadena; i++){
+  ConteoRegresivo -= 1;
+  cadenaInvertida += cadenaTexto[ConteoRegresivo];
+}
+console.log(cadenaInvertida);
 
-const cadenaInvertida = '';
-const indice = longitudCadena; 
 
-for(let i = 0; i == longitudCadena; i++){
-  cadenaInvertida = cadenaInvertida.concat(cadenaTexto, indice, 1)
-  indice = indice-1;
-} 
+cadenaInvertida = '';
+for(let i = longitudCadena - 1; i >= 0; i--){
+  cadenaInvertida += cadenaTexto[i];
+}
+console.log(cadenaInvertida);
+
+
+
+cadenaInvertida = '';
+console.clear();
+console.log("while: ")
+longitudCadena = cadenaTexto.length;
+
+while (longitudCadena > 0) {
+  longitudCadena -= 1;
+  cadenaInvertida += cadenaTexto[longitudCadena];  
+}
+
 console.log(cadenaInvertida)
