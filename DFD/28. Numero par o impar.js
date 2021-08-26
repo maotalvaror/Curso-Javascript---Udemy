c9970cab4567413258a8850b0f5c887480cb3d09
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-08-26 18:02:31 -0500

    Programa que me dice si el numero que ingrese es par o impar

---
+++ DFD/28. Numero par o impar.js
@@ -0,0 +1,7 @@
+console.log('Programa que recibe un numero y dice si es par o impar')
+var num = parseInt(prompt('Digite un numero'));
+if ( num % 2 == 0) {
+  console.log('El numero',num,' que digito es Par');
+} else {
+  console.log('El numero',num,' que digito es Impar');
+}
