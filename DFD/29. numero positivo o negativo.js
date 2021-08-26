71f2ba0f21e76d782dffa2e131159328d234c72b
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-08-26 18:10:36 -0500

    Numero positivo o negativo segun el que digite

---
+++ DFD/29. numero positivo o negativo.js
@@ -0,0 +1,10 @@
+console.log('Programa que me dice si un Numero positivo o negativo')
+var numero = parseInt(prompt('Digite un numero'));
+
+if (numero == 0){
+  console.log('El numero es cero')
+}else if ( numero > 0 ) {
+  console.log('Es positivo');
+} else {
+  console.log('Es negativo');
+}
