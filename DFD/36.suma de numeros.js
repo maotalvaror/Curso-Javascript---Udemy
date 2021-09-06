9da8576d3097cc0d2f7a7d889a953e7309067ae1
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-09-06 16:41:28 -0500

    Suma de numeros

---
+++ DFD/36.suma de numeros.js
@@ -0,0 +1,9 @@
+var n1 = parseInt(prompt('Digite el primer numero'));
+var n2 = parseInt(prompt('Digite el segundo numero')); 
+var n3 = parseInt(prompt('Digite el tercer numero'));
+
+if (n1 + n2 == n3) {
+  console.log('Se cumple ya que el numero',n1,'y el numero',n2,'es',n3)
+} else {
+  console.log('No se cumple')
+}
