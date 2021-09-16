427352446bc33b3049ff632dd5ec758b523fd944
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-09-16 18:44:09 -0500

    Analizando valores

---
+++ DFD/41.analizando dfd.js
@@ -0,0 +1,6 @@
+//primer video de dfd 
+var x = parseInt(prompt('Digite el valor de x'));
+var y = parseInt(prompt('Digite el valor de y'));
+var z = prompt('Digite una palabra');
+
+console.log('El valor de x es',x,'el de y es',y,'la palabra digitada con el valor de z es',z)
