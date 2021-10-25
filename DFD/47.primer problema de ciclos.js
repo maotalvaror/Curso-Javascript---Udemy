225480155c4b778b56b3427fef2a8b9b485826b8
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-10-25 17:33:29 -0500

    Primer problema en ciclos

---
+++ DFD/47.primer problema de ciclos.js
@@ -0,0 +1,6 @@
+var numero = parseInt(prompt('Digite un numero'));
+var suma = 0;
+
+for (let i = 1; i <= numero; i++) {
+  suma = suma + i;
+} console.log('La suma de: ',numero,' elementos es: ' ,suma)
