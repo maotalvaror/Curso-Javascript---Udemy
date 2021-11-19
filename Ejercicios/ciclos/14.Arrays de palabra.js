35c4588abd7636bd956e8b01a2f1dc704eb1be8a
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-11-19 08:43:07 -0500

    Array de palabras

--- Ejercicios/ciclos/14.Arrays de palabra.js
+++ Ejercicios/ciclos/14.Arrays de palabra.js
@@ -14,6 +14,6 @@
 function llamarFuncion(arr) {
   for (let i = 0; i < arr.length; i++) {
     var M = arr[i][0].toUpperCase() + arr[i].substring(1, arr[i].length).toLowerCase();
-    console.log(M)
+    console.log(M);
   }
 }
