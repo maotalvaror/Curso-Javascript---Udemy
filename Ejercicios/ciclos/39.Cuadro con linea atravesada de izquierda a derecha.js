5823309ad220cd96be5b474567cf659fee0961e5
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-12-13 08:22:20 -0500

    Cuadro con linea atravesada de IZ a DR

---
+++ Ejercicios/ciclos/39.Cuadro con linea atravesada de izquierda a derecha.js
@@ -0,0 +1,15 @@
+function llamarFuncion(numero) {
+  var concatenacionAsteriscos = '';
+  for (let i = 1; i <= numero; i++) {
+    for (let j = 0; j < numero; j++){
+      if (j == (numero-1) || j == 0 || i == numero || i == 1 || i == j){
+        concatenacionAsteriscos += '*';
+      } else {
+        concatenacionAsteriscos += ' ';
+      }
+    }
+    concatenacionAsteriscos += '\n'
+  }
+  console.log(concatenacionAsteriscos)
+    
+}
