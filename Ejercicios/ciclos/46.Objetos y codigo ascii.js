621356be71a7eeeb7c6f9b7ddcd3577750982582
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2021-12-14 12:45:29 -0500

    Codigo ascii y objetos

---
+++ Ejercicios/ciclos/46.Objetos y codigo ascii.js
@@ -0,0 +1,5 @@
+for (let i = 65; i <= 90; i++){
+  var Letra = String.fromCharCode(i);
+  console.log([Letra] + ': ' + [i]); // A: 65
+  console.log(`Letra: ${Letra} Numero: ${i}`) // Letra: A Numero: 65
+}
