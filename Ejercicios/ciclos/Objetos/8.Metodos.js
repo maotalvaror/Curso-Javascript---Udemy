7c16aaafb4f2b83c451a8be3cd916f80158fe467
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-03-08 09:01:29 -0500

    Metodos de objetos

---
+++ Ejercicios/ciclos/Objetos/8.Metodos.js
@@ -0,0 +1,33 @@
+/*
+function Nombre_Edad(primer_nombre, Edad) {
+ 
+  //this = Durante la ejecucion this es el objeto user (es el "objeto actual")
+ 
+
+  this.primer_nombre = primer_nombre;
+  this.Edad = Edad;
+}
+
+//new = crea un tipo de objeto definido por el usuario
+
+var persona_A = new Nombre_Edad("Maria", 14);
+var persona_B = new Nombre_Edad("Yeison", 33);
+var persona_C = new Nombre_Edad("Elizabeth", 52);
+
+console.table([persona_A, persona_B, persona_C]);
+*/
+
+
+/*
+El método .push() = añade uno o más elementos al final de un array 
+y devuelve la nueva longitud del array.
+
+
+El método toString() = se llama automáticamente cuando el objeto se
+representa como un valor de texto.
+
+
+El método .map() = llama a una funcion para cada elemento de un array, 
+retorna lo que el usuario desee.
+
+*/
