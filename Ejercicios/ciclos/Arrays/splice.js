da00c359ee14f8dcdc25672d8bf61fcc1eeba0c3
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:51:07 -0500

    splice

---
+++ Ejercicios/ciclos/Arrays/splice.js
@@ -0,0 +1,7 @@
+// splice : Para eliminar un elemento mediante su posicion.
+
+var frutas = ['Manzana', 'Pera', 'Mango'];
+
+let eliminar_Elemento = frutas.splice(posicion, 1);
+console.log(eliminar_Elemento); // se elimina 'Pera'.
+console.log(frutas);
