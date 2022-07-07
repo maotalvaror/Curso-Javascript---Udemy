1f2df8502a0b69bba5ec5bf7ac6033f998897944
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:48:06 -0500

    find

---
+++ Ejercicios/ciclos/Arrays/find.js
@@ -0,0 +1,6 @@
+/* find : Devuelve el valor del primer elemento que cumpla
+con la condicion dada.*/
+
+const numeros = [1, 4, 8, 9, 3, 5, 7, 2, 6];
+const nuevo_arr_numeros = numeros.find( num => num > 4);
+console.log(nuevo_arr_numeros);
