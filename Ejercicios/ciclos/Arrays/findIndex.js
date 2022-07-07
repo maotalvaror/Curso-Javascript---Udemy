31511bbf485dd70a9c7a559cff79ebc068b4b172
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:48:21 -0500

    findIndex

---
+++ Ejercicios/ciclos/Arrays/findIndex.js
@@ -0,0 +1,4 @@
+// findIndex : devuelve el indice del primer elemento de un array que cumpla con la funcion
+const num = [103, 475, 821, 93, 36, 52, 76, 2, 666];
+const nuevo_arr_num = ( num ) => num > 300;
+console.log(num.findIndex(nuevo_arr_num));
