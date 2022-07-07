1f58a30f640dc05a1b2ee6b0a6f040ca65327339
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:51:19 -0500

    unshift

---
+++ Ejercicios/ciclos/Arrays/unshift.js
@@ -0,0 +1,6 @@
+// unshift : Para añadir un elemento al principio del array.
+
+var frutas = ['Manzana', 'Pera', 'Mango'];
+
+let nuevaLongitud_P = frutas.unshift('Durazno');
+console.log(frutas, nuevaLongitud_P);
