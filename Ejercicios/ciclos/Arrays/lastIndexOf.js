21c892ea9f1ea2461eb5c4c637445e2b67fe064d
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:50:01 -0500

    lastIndexOf

---
+++ Ejercicios/ciclos/Arrays/lastIndexOf.js
@@ -0,0 +1,5 @@
+// lastIndexOf : devuelve el indice en el que un cierto elemento puede encontrarse en el arreglo
+
+const animales_ = ['Tigre', 'Puma', 'Leon', 'Oso'];
+console.log(animales_.lastIndexOf('Puma')); //1
+console.log(animales_.lastIndexOf('Oso')); //3
