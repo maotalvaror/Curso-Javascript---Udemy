7051900c78796aee7f1d204ecd50bd359c21c68b
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:50:56 -0500

    slice

---
+++ Ejercicios/ciclos/Arrays/slice.js
@@ -0,0 +1,7 @@
+// slice : Para copiar un array.
+
+var frutas = ['Manzana', 'Pera', 'Mango'];
+
+let copia_Array_Frutas = frutas.slice();
+console.log(copia_Array_Frutas, 'Copia');
+console.log(frutas, 'Original');
