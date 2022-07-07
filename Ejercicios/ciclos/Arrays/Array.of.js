d9b17577a3219ec6a4d3fb61807c27e7f820a43d
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:47:13 -0500

    Array.of

---
+++ Ejercicios/ciclos/Arrays/Array.of.js
@@ -0,0 +1,4 @@
+// Array.of() : crea una nueva instancia Array con un número variable de elementos pasados como argumentos.
+
+let Array_of = Array.of('Y', 'A', 'D', true, false);
+console.log(Array_of);
