f49b31b00f49762e30184737570db795c55450ec
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:48:55 -0500

    incluides

---
+++ Ejercicios/ciclos/Arrays/incluides.js
@@ -0,0 +1,4 @@
+// incluides : verifica si el elemento esta incluido en el array, devuelve true o false según corresponda.
+
+let mascotas = ['Perro', 'Gato', 'Pajaro'];
+console.log(mascotas.includes('Perro'));
