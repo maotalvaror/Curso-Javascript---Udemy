05fd578091f7330c9e584105847466c09d28fee3
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:50:35 -0500

    push

---
+++ Ejercicios/ciclos/Arrays/push.js
@@ -0,0 +1,6 @@
+// push : Para añadir un elemento al final del array.
+
+var frutas = ['Manzana', 'Pera', 'Mango'];
+
+let nuevaLongitud_F = frutas.push('Fresa');
+console.log(frutas, nuevaLongitud_F);
