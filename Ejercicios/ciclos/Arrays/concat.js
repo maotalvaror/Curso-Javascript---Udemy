6ce1cbaccf7c29ba26515673e8d4c939638b331c
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-07-07 16:47:31 -0500

    concat

---
+++ Ejercicios/ciclos/Arrays/concat.js
@@ -0,0 +1,6 @@
+// concat : se usa para unir dos o mas arrays.
+
+let balones = ['Futbol', 'Voleibol'];
+let balones_2 = ['Beisbol', 'Tenis'];
+let concatenacion = balones.concat(balones_2);
+console.log(concatenacion);
