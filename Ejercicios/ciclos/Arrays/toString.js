6f9deabf28ed4aafd6fc785bae7d6b882e762a47
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-08-30 09:15:38 -0500

    toString

---
+++ Ejercicios/ciclos/Arrays/toString.js
@@ -0,0 +1,7 @@
+//devuelve una cadena de caracteres representando el array especificado y sus elementos
+const array = [0, "6", "02", [2], 0, "20"];
+console.log(array.toString());
+
+
+const frutas = ["Manzana", " Pera", " Uva"];
+console.log(frutas.toString());
