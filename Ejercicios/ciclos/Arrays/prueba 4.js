bb3268c3d6abf7b06c7adb10b1ff64f2942f735d
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-10-10 15:13:27 -0500

    Prueba 4 de arrays

---
+++ Ejercicios/ciclos/Arrays/prueba 4.js
@@ -0,0 +1,21 @@
+var a = ["H", "O"];
+var agregar = a.push("L", "A");
+console.log(a);
+
+console.log(a.pop());
+console.log(a);
+
+console.log(a.unshift("a"));
+console.log(a);
+
+console.log(a.shift());
+console.log(a);
+
+console.log(a.unshift("¡"));
+console.log(a);
+
+console.log(a.push("A"));
+console.log(a);
+
+console.log(a.push("!"));
+console.log(a);
