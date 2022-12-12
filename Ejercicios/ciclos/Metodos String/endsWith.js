6ce58450776118931a0c1b258e24895d31da34fb
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 08:37:58 -0500

    endsWith

---
+++ Ejercicios/ciclos/Metodos String/endsWith.js
@@ -0,0 +1,7 @@
+//valor boolean si termina con ('z')
+
+var Nombre = 'Yeison Rodriguez';
+console.log(Nombre.endsWith('z')); // true
+
+var pregunta = '¿Que edad tiene';
+console.log(pregunta.endsWith('?')); // false
