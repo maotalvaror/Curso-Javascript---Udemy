075fa6b9de83a1a6b90e7f7d938d7174e24bc98e
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 10:47:32 -0500

    slice

---
+++ Ejercicios/ciclos/Metodos String/slice.js
@@ -0,0 +1,5 @@
+//Crea un string nuevo desde la posicion enviada 
+// 'Yeison no se baña' (7) = no se baña
+var string_original = 'Yeison no se baña';
+var nueva_copia = string_original.slice(7);
+console.log(nueva_copia)
