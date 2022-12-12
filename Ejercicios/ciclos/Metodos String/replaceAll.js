d42ae5e18bb2d42b25a6c276f5f66d82b06a200f
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 10:18:39 -0500

    replaceAll

---
+++ Ejercicios/ciclos/Metodos String/replaceAll.js
@@ -0,0 +1,4 @@
+// Reemplaza todo lo que sea 'dog' por 'monkey' en este caso fueron dos veces
+var cadena = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+var Reemplazar = cadena.replaceAll('dog', 'monkey');
+console.log(Reemplazar);
