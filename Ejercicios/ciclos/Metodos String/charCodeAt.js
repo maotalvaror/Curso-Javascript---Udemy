cb440af4d9b8996dddcc41400282296e81f726ba
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 08:36:52 -0500

    charCodeAt

---
+++ Ejercicios/ciclos/Metodos String/charCodeAt.js
@@ -0,0 +1,7 @@
+//Busca en el codigo ascii segun la letra que se esta enviando
+var letras = 'ABCDEFGHI';
+var CodigoAscii = letras.charCodeAt(4);
+console.log(CodigoAscii); // D == 69
+
+var Nombre = 'Alejandra';
+console.log(Nombre.charCodeAt(5)); // n == 110
