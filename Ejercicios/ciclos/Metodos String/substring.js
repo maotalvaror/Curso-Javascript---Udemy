2e71f78738f72d6f74daafcb727523e8612a321c
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 11:10:19 -0500

    substring

---
+++ Ejercicios/ciclos/Metodos String/substring.js
@@ -0,0 +1,6 @@
+//Muestra los caracteres segun los indices que se le envian de la cadena "Otalvaro";
+var cualquierCadena = "Otalvaro";
+console.log(cualquierCadena.substring(0,4)); // Ota
+console.log(cualquierCadena.substring(4,8)); // var
+console.log(cualquierCadena.substring(0,6)); // Otalva
+console.log(cualquierCadena.substring(0,8)); // Otalvaro
