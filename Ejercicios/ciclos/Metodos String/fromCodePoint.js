c26d4ce37ad052d43bfcfc499f408d56e616bb29
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 08:38:34 -0500

    fromCodePoint

---
+++ Ejercicios/ciclos/Metodos String/fromCodePoint.js
@@ -0,0 +1,6 @@
+var codigos = String.fromCodePoint(9738, 9793, 9792, 0x2F807, 92);
+console.log(codigos); // ☊♁♀倂\
+
+var codigos_2 = String.fromCodePoint(9128, 9093, 9672, 0x2F802);
+console.log(codigos_2); // ⎨⎅◈乁
+
