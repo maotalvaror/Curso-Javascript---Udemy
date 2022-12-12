1766ee9d16275951ef5c95e2770a597f8fa9ede8
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 10:59:36 -0500

    startsWith

---
+++ Ejercicios/ciclos/Metodos String/startsWith.js
@@ -0,0 +1,6 @@
+//Retorna un valor boolean si inicia con ('INGRESADO');
+var cadena = 'Dana esta estudiando';
+console.log(cadena.startsWith('D')); // true
+
+var pregunta = '¿Que edad tiene?';
+console.log(pregunta.endsWith('Q')); // false = inicia con ' ¿ '
