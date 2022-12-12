a4ea7425e67cd3be47474996facdf925dede33ee
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 09:02:14 -0500

    indexOf

---
+++ Ejercicios/ciclos/Metodos String/indexOf.js
@@ -0,0 +1,7 @@
+var nombre = "maria alejandra";
+var indice = nombre.indexOf('d');
+console.log( indice ); // 12 
+
+var apellido = 'otalvaro rodriguez';
+var indice_2 = apellido.indexOf('r'); // 6
+console.log(indice_2);
