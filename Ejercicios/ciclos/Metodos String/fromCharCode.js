79c983f0a592649b330688ff402b851e2ef31621
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 08:38:17 -0500

    fromCharCode

---
+++ Ejercicios/ciclos/Metodos String/fromCharCode.js
@@ -0,0 +1,5 @@
+var nombre = String.fromCharCode(77, 97, 114, 105, 97);
+console.log(nombre); // Maria
+
+var apellido = String.fromCharCode(79, 116, 97, 108, 118, 97, 114, 111);
+console.log(apellido); // Otalvaro
