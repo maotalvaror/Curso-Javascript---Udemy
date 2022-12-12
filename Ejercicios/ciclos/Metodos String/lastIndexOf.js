24c06cbb04bd92f0cfbddb3651d332411c6fdc19
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-12 09:02:34 -0500

    lastIndexOf

---
+++ Ejercicios/ciclos/Metodos String/lastIndexOf.js
@@ -0,0 +1,7 @@
+var cadena = 'Ballena Azul';
+var indice = cadena.lastIndexOf('a');
+console.log(indice);  // 6
+
+var pregunta = '¿Como se llama?';
+var indice_2 = pregunta.lastIndexOf('a');
+console.log(indice_2); // 13
