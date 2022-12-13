a9b473b201332cc61c812236c27c8a85a30e87a3
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-13 09:21:10 -0500

    trim

---
+++ Ejercicios/ciclos/Metodos String/trim.js
@@ -0,0 +1,8 @@
+//elimina los espacios en blanco, solamente en los extremos
+var nombre = ' Maria   ';
+var eliminar_espacios = nombre.trim();
+console.log(eliminar_espacios);
+
+var fecha = '  Martes 13 de diciembre de 2022  ';
+var elimina_espacio = fecha.trim();
+console.log(elimina_espacio);
