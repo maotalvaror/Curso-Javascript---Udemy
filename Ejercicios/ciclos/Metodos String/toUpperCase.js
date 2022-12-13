a6879b7cb0e5f081febcd1fc72a8288ee9dd8908
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-13 08:46:44 -0500

    toUpperCase

---
+++ Ejercicios/ciclos/Metodos String/toUpperCase.js
@@ -0,0 +1,10 @@
+//convertir la cadena en mayusculas
+//'trece de diciembre de 2022'; === 'TRECE DE DICIEMBRE DE 2022';
+
+var cadena = 'trece de diciembre de 2022';
+var mayusculas = cadena.toUpperCase();
+console.log(mayusculas);
+
+var dias = 'lunes, martes, miercoles, jueves, viernes';
+var mayusculas_ = dias.toUpperCase();
+console.log(mayusculas_);
