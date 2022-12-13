63172515d6aea82a29e81707d995ab6c729b97cb
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-13 09:21:23 -0500

    trimEnd

---
+++ Ejercicios/ciclos/Metodos String/trimEnd.js
@@ -0,0 +1,7 @@
+//elimina los espacios al final del string
+var fecha = '                 MARTES 13 DE DICIEMBRE DE 2022    ';
+var espacios_final = fecha.trimEnd();
+console.log(espacios_final);
+
+var nombre = '            .Yeral.             ';
+console.log(nombre.trimEnd());
