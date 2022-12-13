224d705856bf5fb2a62439c45050a3569a5605a0
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-13 08:46:34 -0500

    toLowerCase

---
+++ Ejercicios/ciclos/Metodos String/toLowerCase.js
@@ -0,0 +1,10 @@
+//convertir la cadena en minusculas
+//'ONCE DE DICIEMBRE DE 2022'; === 'once de diciembre de 2022';
+
+var cadena = 'ONCE DE DICIEMBRE DE 2022';
+var minusculas = cadena.toLowerCase();
+console.log(minusculas);
+
+var dia = 'LUNES, MARTES, MIERCOLES, JUEVES, VIERNES';
+var minusculas_ = dia.toLowerCase();
+console.log(minusculas_)
