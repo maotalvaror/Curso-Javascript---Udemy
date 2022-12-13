9558ad449f7d4a26f26b3dd12ec11a001db24976
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-13 09:21:31 -0500

    trimStart

---
+++ Ejercicios/ciclos/Metodos String/trimStart.js
@@ -0,0 +1,7 @@
+//elimina los espacios al inicio del string
+var fecha = '                 MARTES 13 DE DICIEMBRE DE 2022    ';
+var espacios_inicio = fecha.trimStart();
+console.log(espacios_inicio);
+
+var nombre = '            .Yeral.             ';
+console.log(nombre.trimStart());
