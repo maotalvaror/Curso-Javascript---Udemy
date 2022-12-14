7344152b61805834613ec64d41f779c40be03c41
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-14 10:23:13 -0500

    split

---
+++ Ejercicios/ciclos/Metodos String/split.js
@@ -0,0 +1,13 @@
+// El metodo split divide o retorna una parte del string especificado
+
+var frase = 'Yeison no se baña';
+var dividir = frase.split(' ');
+console.log(dividir[1]);
+
+var frase_2 = 'Yeraldin no se baña';
+var dividir = frase.split('');
+console.log(dividir[0]);
+
+var frase_3 = 'Elizabeth no se baña';
+var dividir = frase.split();
+console.log(dividir);
