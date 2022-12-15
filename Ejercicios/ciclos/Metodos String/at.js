929218715e725c5bd177e5ab0580e2aa0d7295b8
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-15 09:13:26 -0500

    at

---
+++ Ejercicios/ciclos/Metodos String/at.js
@@ -0,0 +1,9 @@
+var str = 'The quick brown fox jumps over the lazy dog';
+var indice = -5;
+console.log(str.at(indice)); // y
+
+indice = 13;
+console.log(str.at(indice)); // w
+
+indice = -20;
+console.log(str.at(indice)); // p
