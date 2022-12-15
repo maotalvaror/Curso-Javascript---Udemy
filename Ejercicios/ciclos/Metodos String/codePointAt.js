f2473c8447ef308ebb2ce08ecc2d19a8b6272daa
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-15 09:38:52 -0500

    codePointAt

---
+++ Ejercicios/ciclos/Metodos String/codePointAt.js
@@ -0,0 +1,9 @@
+var simbolos = '☊♁倂♀';
+var codigos = simbolos.codePointAt(1);
+console.log(codigos);
+
+var codigos_ = simbolos.codePointAt(0);
+console.log(codigos_);
+
+simbolos = '😂😇😋😎😍';
+console.log(simbolos.codePointAt(4));
