9fed0d4913f993e338373dca0a0615f6c4ced316
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-21 12:53:46 -0500

    getDay

---
+++ Ejercicios/ciclos/Date/getDay.js
@@ -0,0 +1,17 @@
+// Devuelve el dia de la semana
+/*
+
+1 = Lunes
+2 = Martes
+3 = Miercoles
+4 = Jueves
+5 = Viernes
+6 = Sabado
+7 = Domingo
+
+*/
+
+var fecha = new Date('December 24, 2022 11:59:58');
+var semana_ = fecha.getDay();
+
+console.log(semana_); // 6
