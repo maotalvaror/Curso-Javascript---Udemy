e8e190818789f08f162abd93acdf25846d33e8d8
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-22 08:45:49 -0500

    parse

---
+++ Ejercicios/ciclos/Date/parse.js
@@ -0,0 +1,3 @@
+// Transforma una cadena de fecha y devuelve el número de milisegundos desde las 00:00:00 del 1 de enero de 1970, hora local.
+var fecha = Date.parse('December 22, 2022');
+console.log(fecha);
