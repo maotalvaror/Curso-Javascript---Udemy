f240dd953801df62950b2fa300b50c39a1fc70df
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:18:15 -0500

    Math.floor

---
+++ Ejercicios/ciclos/MATH/Math.floor.js
@@ -0,0 +1,14 @@
+/*
+
+DEVUELVE EL MÁXIMO ENTERO MENOR O IGUAL A UN NÚMERO
+
+  Math.floor(5.96); = 5
+  Math.floor(-2.334); = -3
+  Math.floor(-0.98886); = -1
+
+*/
+
+function numeroEnteroMenor(num){
+  return Math.floor(num);
+}
+numeroEnteroMenor();
