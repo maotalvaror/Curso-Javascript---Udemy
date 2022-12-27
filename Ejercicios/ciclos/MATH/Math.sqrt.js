802501c4a1fc07473f747671ad89820a4ce6041e
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:19:21 -0500

    Math.sqrt

---
+++ Ejercicios/ciclos/MATH/Math.sqrt.js
@@ -0,0 +1,15 @@
+/*
+
+DEVUELVE LA RAIZ CUADRADA DE EL NUMERO QUE SE LE ENVIA A LA FUNCION raizCuadrada
+
+  Math.sqrt(4); = 2
+  Math.sqrt(25); = 5
+  Math.sqrt(9); = 3
+  Math.sqrt(49); = 7
+
+*/
+
+
+function raizCuadrada(num){
+  return Math.sqrt(num);
+}
