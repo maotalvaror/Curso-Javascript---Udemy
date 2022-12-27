99bdddc38891f660baf3e9eacf7f6e44e534daf3
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:42:38 -0500

    Math.imul

---
+++ Ejercicios/ciclos/MATH/Math.imul.js
@@ -0,0 +1,14 @@
+/*
+
+DEVUELVE UNA MULTIPLICACION ENTRE DOS PARAMETROS, EN ESTE CASO A Y B
+
+  Math.imul(8, 2); = 16
+  Math.imul(-5, 7); = -35
+  Math.imul(-4, -4); = 16
+  Math.imul(-3, -9); = 27
+
+*/
+
+function multiplicar(a, b){
+  return Math.imul(a, b);
+}
