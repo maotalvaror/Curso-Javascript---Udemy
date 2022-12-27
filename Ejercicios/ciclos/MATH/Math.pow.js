37e99db65058c280b2a0ade33eb7f9d326ab9c5a
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:19:01 -0500

    Math.pow

---
+++ Ejercicios/ciclos/MATH/Math.pow.js
@@ -0,0 +1,13 @@
+/*
+
+DEVUELVE LA BASE ELEVADA AL EXPONENTE
+
+  Math.pow(3, 3); = 3*3*3 = 27
+  Math.pow(8, 6); = 8*8*8*8*8*8 = 262144
+  Math.pow(2, 5); = 2*2*2*2*2 = = 32
+
+*/
+
+function baseExponente(base, exp){
+  return Math.pow(base, exp);
+}
