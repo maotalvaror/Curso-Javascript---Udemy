1d909a01ef48d99cb77658f61005ef103b7ed0a9
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:17:58 -0500

    Math.exp

---
+++ Ejercicios/ciclos/MATH/Math.exp.js
@@ -0,0 +1,17 @@
+/*
+
+DEVUELVE ex DONDE X ES EL ARGUMENTO Y E ES EL NUMERO DE EULER
+
+  𝙼𝚊𝚝𝚑.𝚎𝚡𝚙 ( 𝚡 ) = e x 
+
+  Math.exp(-0); = 1
+  Math.exp(1); = 2.718281828459045
+  Math.exp(-2); = 0.1353352832366127
+
+*/
+
+function exponente(num){
+  return Math.exp(num);
+}
+
+exponente();
