64e4b0003d6d1a104390cfecf84f46c3faa5632e
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:18:50 -0500

    Math.PI

---
+++ Ejercicios/ciclos/MATH/Math.PI.js
@@ -0,0 +1,13 @@
+/*
+
+REPRESENTA LA RELACIÓN ENTRE LA LONGITUD DE LA CIRCUNFERENCIA DE UN CIRCULO Y SU DIAMETRO,
+LA CUAL ES APROXIMADAMENTE 3.14159.
+
+  Math.PI = π ≈ 3.14159.
+
+*/
+function calcularCircunferencia(num_veces, radio){
+  return num_veces * Math.PI * radio;
+}
+
+calcularCircunferencia();
