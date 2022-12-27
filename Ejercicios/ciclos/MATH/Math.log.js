2047e0c6eb57696d87706ec47664dbedf064f41c
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 13:10:36 -0500

    Math.log

---
+++ Ejercicios/ciclos/MATH/Math.log.js
@@ -0,0 +1,14 @@
+/*
+
+DEVUELVE LA BASE NEUTRAL DE UN NUMERO
+
+  Multiplica el 2 la cantidad de veces necesarias hasta que de como resultado 8
+  en este caso son 3 veces
+  
+  Math.log(2, 8); = 2*2*2 = 8 = 3
+
+*/
+
+function BaseNeutral(x, y){
+  return Math.log(x) / Math.log(y);
+}
