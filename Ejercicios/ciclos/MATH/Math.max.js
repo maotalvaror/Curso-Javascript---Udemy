598d5e872eff17ef1497f2c7fc5d4b843417f316
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:18:29 -0500

    Math.max

---
+++ Ejercicios/ciclos/MATH/Math.max.js
@@ -0,0 +1,15 @@
+/*
+
+DEVUELVE EL NUMERO MAYOR DE CERO O MAS NUMEROS DADOS
+
+  Math.max(1, 3, 2); // 3
+  Math.max(-1, -3, -2); // -1
+  
+  const array1 = [1, 3, 2];
+  Math.max(...array1); // 3
+
+*/
+
+function numeroMayorACero(){
+  return Math.max(1, 473, 1838128, 122312, 32322);
+}
