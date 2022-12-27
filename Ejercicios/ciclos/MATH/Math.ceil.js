3fb845431c3bff8c19dd35db7824aa75bed7bbbb
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2022-12-27 12:17:49 -0500

    Math.ceil

---
+++ Ejercicios/ciclos/MATH/Math.ceil.js
@@ -0,0 +1,14 @@
+/*
+
+DEVUELVE EL ENTERO MAYOR O IGUAL MAS PRÓXIMO A UN NÚMERO DADO.
+
+  Math.ceil( 0.84 ); = 1
+  Math.ceil(79.004); = 80
+  Math.ceil(4.92); = 5
+
+*/
+
+function numeroEnteroMayor(num){
+  return Math.ceil(num);
+}
+numeroEnteroMayor();
