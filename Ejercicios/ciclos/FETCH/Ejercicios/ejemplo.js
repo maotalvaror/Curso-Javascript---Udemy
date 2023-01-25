492d5f8f866a534b719cdffecf344efeb618100f
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2023-01-25 16:27:44 -0500

    Ejemplos redes sociales

---
+++ Ejercicios/ciclos/FETCH/Ejercicios/ejemplo.js
@@ -0,0 +1,23 @@
+// function fn(){
+    //   var ul;
+
+    //   ul = document.createElement('ul');
+    //   ul.classList.add("lista-fb", "d-inline-block");
+
+    //   var arr = ["Instagram", "Facebook", "Whatsapp"];
+
+    //   for(let i = 0; i < arr.length; i++){
+    //     var li, i;
+
+    //     li = document.createElement('li');
+    //     li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+    //     li.innerText = arr[i];
+
+    //     i = document.createElement('i');
+    //     i.classList.add("fa-brands", `fa-instagram`, "ms-3", "fs-3");
+    //     li.appendChild(i);
+    //     ul.appendChild(li);
+    //   }
+      
+    //   document.body.appendChild(ul);
+    // }s
