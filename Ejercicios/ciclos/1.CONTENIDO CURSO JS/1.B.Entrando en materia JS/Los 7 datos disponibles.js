fa30402fc30875c94a4a4b4425b3ccb98e0649a5
Maria Otalvaro <otalvaroalejandra03@gmail.com>
2023-06-27 16:44:40 -0500

    Los 7 datos disponibles

---
+++ Ejercicios/ciclos/1.CONTENIDO CURSO JS/1.B.Entrando en materia JS/Los 7 datos disponibles.js
@@ -0,0 +1,20 @@
+var str = "abc"; 
+console.log(typeof str); // string
+
+var num = 789;
+console.log(typeof num); // number
+
+var obj = {x:3, y:2};
+console.log(typeof obj); // object
+
+var X;
+console.log(typeof X); // undefined
+
+var fn = function(){}
+console.log(typeof fn); // function
+
+var xyz = null
+console.log(typeof xyz); // object
+
+var verdad = true;
+console.log(typeof verdad) // boolean
